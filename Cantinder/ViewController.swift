f196e8e9870e798823f28c48cdcabdad45f05538
Raiman <wijayakaleb@gmail.com>
2019-08-21 15:17:24 +0700

    Initial Commit

---
+++ Cantinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cantinder
+//
+//  Created by Kaleb Wijaya on 21/08/19.
+//  Copyright © 2019 Kaleb Wijaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
